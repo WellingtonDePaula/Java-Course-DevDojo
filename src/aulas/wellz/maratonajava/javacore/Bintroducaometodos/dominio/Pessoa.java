27df1096d9a1840c2e3fd20e288fcf28622907aa
WellingtonDePaula <wellingtonespiao@gmail.com>
2024-06-13 14:00:22 -0300

    Aprendendo sobre o modificador de acesso private

---
+++ src/aulas/wellz/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
@@ -0,0 +1,18 @@
+package aulas.wellz.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ src/aulas/wellz/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,13 @@
+package aulas.wellz.maratonajava.javacore.Bintroducaometodos.test;
+
+import aulas.wellz.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("Wellington");
+        pessoa.setIdade(15);
+        pessoa.imprime();
+    }
+}
